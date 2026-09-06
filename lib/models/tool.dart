9feac1c0f826113d03ca1ed9140fa5de718da36d
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-06 18:42:47 +0200

    Add standard and scientific calculator tool types

--- lib/models/tool.dart
+++ lib/models/tool.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-enum ToolType { percentage, discount, iva, tip, loan, compoundInterest, age, dateDifference, workHours, countdown, area, paint, electricity, fuel, costPerKm, length, weight, bmi, gradeAverage, ruleOfThree }
+enum ToolType { percentage, discount, iva, tip, loan, compoundInterest, age, dateDifference, workHours, countdown, area, paint, electricity, fuel, costPerKm, length, weight, bmi, gradeAverage, ruleOfThree, calculator, scientificCalculator }
 
 class UtiliaTool {
   const UtiliaTool({required this.name, required this.description, required this.category, required this.icon, required this.tint, required this.type});
