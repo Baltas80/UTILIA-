0c84356e471919e097b9e63398638231e5cadf10
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-06 11:15:34 +0200

    refactor: add reusable tool model

---
+++ lib/models/tool.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+enum ToolType { percentage, discount, iva, tip, loan, compoundInterest, age, dateDifference, workHours, countdown, area, paint, electricity, fuel, costPerKm, length, weight, bmi, gradeAverage, ruleOfThree }
+
+class UtiliaTool {
+  const UtiliaTool({required this.name, required this.description, required this.category, required this.icon, required this.tint, required this.type});
+  final String name;
+  final String description;
+  final String category;
+  final IconData icon;
+  final Color tint;
+  final ToolType type;
+}
